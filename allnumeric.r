88508cdb3ba63e639e3c6d502a0a23569ffaa129
Adolfo Hoyos <fitohoyos@gmail.com>
2016-10-08 22:59:29 -0500

    reorganization again

--- allnumeric.r
+++
@@ -1,6 +0,0 @@
-allnumeric <- function(dataset){
-  out <- dataset
-  # out$Gender <- as.numeric(as.character(dataset$Gender))
-  out[ncol(dataset)] <- as.numeric(as.character(dataset[,ncol(dataset)]))
-  return(out)
-}
--- ejecutar.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-condor_submit loaddata.sub
