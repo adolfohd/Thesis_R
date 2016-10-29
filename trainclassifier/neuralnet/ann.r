5728443fc180d3c156c381daf936f646d359c889
Adolfo Hoyos <adolfo.hoyos@correounivalle.edu.co>
2016-10-29 11:25:37 -0500

    quick fix

---
+++ trainclassifier/neuralnet/ann.r
@@ -0,0 +1,56 @@
+
+is.this.cluster = FALSE
+
+# ANN2 - nnet  ###################################################
+
+  # https://beckmw.wordpress.com/tag/nnet/
+
+# ANN - neuralnet  ###################################################
+
+# install.packages("neuralnet")
+# install.packages("NeuralNetTools")
+
+# https://beckmw.wordpress.com/tag/neural-network/
+ 
+library(NeuralNetTools)
+
+if (is.this.cluster){
+  data.folder = ""
+  lib.folder =  ""
+}else{
+  setwd("~/code/thesis_R")
+  data.folder = "data/"
+  lib.folder =  "lib/"
+}
+
+rm(list = ls())
+
+load(  paste(data.folder, "data.RData"  , sep = ""))
+source(paste( lib.folder, "allnumeric.r", sep = ""))
+
+# all features are numeric
+
+library(neuralnet)
+rTrainNN <- allnumeric(r.train)
+rTestNN <- allnumeric(r.test)
+lTrainNN <- allnumeric(l.train)
+lTestNN <- allnumeric(l.test)
+
+# separate each output level into separate outputs
+# L-side
+
+source(paste(lib.folder,"separateoutput.r", sep = ""))
+
+r.train.nn <- separate.output(rTrainNN, gait.phases)
+summary(r.train.nn)
+
+n <- names(r.train.nn)
+f <- as.formula(paste( paste(gait.phases, collapse = " + ")   ," ~", paste(n[!n %in% gait.phases], collapse = " + ")))
+nn <- neuralnet(f,data=r.train.nn,hidden=200)
+plot(nn)
+
+pr.nn <- compute(nn,rTrainNN[,-ncol(rTrainNN)])
+
+
+plot(rTrainNN$Right,pr.nn$net.result, col='red',main='Real vs predicted NN',pch=18,cex=0.7)
+abline(0,1,lwd=2)
