0dc0c08603d6692b9c0970cd995a7f893cfb6ac5
Adolfo Hoyos <adolfo.hoyos@correounivalle.edu.co>
2016-11-25 15:03:08 -0500

    integrate to c#

--- processResults/rfc/main.r
+++ processResults/rfc/main.r
@@ -1,29 +1,32 @@
 # --- Dependencies --- #
 # install.packages("randomForest")
-install.packages("plot3D")
+# install.packages("plot3D")
 # install.packages("plotly")
 # -------------------- #
 
-setwd("~/code/thesis_R/processResults/rfc")
+# setwd("~/code/thesis_R/processResults/rfc")
+setwd("~/code/thesis_R/")
 rm(list = ls())
-source("bestrfc.r")
+source("lib/bestrfc.r")
 
-
-
-setwd("~/code/thesis_R/trainclassifier/randomforest")
-
+# setwd("~/code/thesis_R/trainclassifier/randomforest")
 
 library(randomForest)
 
-data.folder = "results/20161007primeraviernes/data/"
-files1 = list.files(data.folder)
+data.folder.left = "data/results/20161012/left/"
+files.left = list.files(data.folder.left)
+best.forest.info.left <- best.rfc(files.left, data.folder.left)
 
-best.forest.info <- best.rfc(files1, data.folder)
+data.folder.right = "data/results/20161012/right/"
+files.right = list.files(data.folder.right)
+best.forest.info.right <- best.rfc(files.right, data.folder.right)
 
-library(plot)
+# library(plot)
 
-x <- 1:nrow(best.forest.info$errors)
-y <- 1:ncol(best.forest.info$errors)
-z <- best.forest.info$errors
+x <- 1:nrow(best.forest.info.left$errors)
+y <- 1:ncol(best.forest.info.left$errors)
+z <- best.forest.info.left$errors
 
-plot3D::persp3D(x,y,z, theta = 135, phi = 40, clab = "Out-of-bag error")
+# plot3D::persp3D(x,y,z, theta = 135, phi = 40, clab = "Out-of-bag error - LEFT")
+# plot3D::persp3D(x,y,z,phi = 45, theta = 45,clab = "LEFT", xlab = 'ntrees', ylab = 'mtry', zlab = 'OOB error', axes = TRUE, ticktype = 'detailed', expand=.8)
+plot3D::persp3D(x,y,z,phi = 45, theta = 45,clab = "LEFT", axes = TRUE, ticktype = 'detailed', expand=.8)
